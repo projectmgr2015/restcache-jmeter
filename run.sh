327707f89305ad3794245cdc38ce99b4779182fb
Marcin Jasion <marcin.jasion@gmail.com>
2015-08-10 16:32:39 +0200

    changed save path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker rm -f restcache-jmeter
-docker run -it -d --name restcache-jmeter -v $(pwd):/root restcache-jmeter:latest bin/jmeter  -n -t /root/$1 -l $2
+docker run -it -d --name restcache-jmeter -v $(pwd):/root restcache-jmeter:latest bin/jmeter  -n -t /root/$1 -l /root/$2
