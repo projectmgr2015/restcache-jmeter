f77c42659778ca5c6416627248a84872e245f65a
Marcin Jasion <marcin.jasion@gmail.com>
2015-10-17 19:10:15 +0200

    refactoring

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+jtl_file=$1
+output_file=$2
+
 docker rm -f restcache-jmeter
-docker run -it -d --name restcache-jmeter -v $(pwd):/root restcache-jmeter:latest bin/jmeter  -n -t /root/$1 -l /root/$2
+docker run -it -d --name restcache-jmeter -v $(pwd):/root restcache-jmeter:latest bin/jmeter  -n -t /root/$jtl_file -l /root/$output_file
